5859a028d84f31fd0ed763e77701b60ce96414e9
h3nse <henrikf.bj@gmail.com>
2023-12-07 18:13:47 +0100

    add obstacle creation

--- main.gd
+++ main.gd
@@ -1,27 +1,47 @@
 extends Node2D
 
-@export var ROWS = 10
-@export var COLS = 10
+@export_group("Grid")
+@export_range(0,50,1,"or_greater") var ROWS = 10
+@export_range(0,50,1,"or_greater") var COLS = 10
 @export var SOURCE = Vector2()
 @export var DESTINATION = Vector2(ROWS-1 , COLS-1)
+@export_range(0,100) var OBSTACLE_RATION = 30
+@export_group("")
 
 var _node = preload("res://pathNode.tscn")
 
 var grid = []
+var isValid = true
 
-func create_grid(rows, cols):
-	for x in rows:
+func create_grid():
+	for x in ROWS:
 		grid.append([])
-		for y in cols:
+		for y in COLS:
 			var node = _node.instantiate()
 			node.init(x,y)
 			grid[x].append(node)
 			add_child(node)
 
+func checkIsValid():
+	return (SOURCE.x >= 0 and SOURCE.x < ROWS and SOURCE.y >= 0 and SOURCE.y < COLS
+	and DESTINATION.x >= 0 and DESTINATION.x < ROWS and DESTINATION.y >= 0 and DESTINATION.y < COLS)
+
+func addObstacles():
+	for x in range(grid.size()):
+		for y in range(grid[x].size()):
+			if randi_range(0,100) < OBSTACLE_RATION:
+				grid[x][y].isObstacle = true
+
 func _ready():
-	create_grid(ROWS, COLS)
-	grid[SOURCE.x][SOURCE.y].modulate = Color(0,1,0)
-	grid[DESTINATION.x][DESTINATION.y].modulate = Color(1,0,0)
+	if not checkIsValid():
+		isValid = false
+	if isValid:
+		create_grid()
+		addObstacles()
+		grid[SOURCE.x][SOURCE.y].modulate = Color(0,1,0)
+		grid[SOURCE.x][SOURCE.y].isObstacle = false
+		grid[DESTINATION.x][DESTINATION.y].modulate = Color(1,0,0)
+		grid[DESTINATION.x][DESTINATION.y].isObstacle = false
 
 
 func _process(delta):
--- pathNode.gd
+++ pathNode.gd
@@ -3,12 +3,16 @@ extends Sprite2D
 var x
 var y
 const gridSpace = 5
+var isObstacle: bool = false:
+	set(new_value):
+		isObstacle = new_value
+		if isObstacle:
+			modulate = Color.BLACK
 
-func init(x, y):
-	self.x = x
-	self.y = y
+func init(_x, _y):
+	x = _x
+	y = _y
 	position = Vector2(x, y) * (scale.x + gridSpace) + Vector2(scale.x, scale.y) / 2
-	CanvasModulate
 
 func _ready():
 	pass
